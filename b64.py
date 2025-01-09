06b864b72d5ab6c6928f747e36185c789921b40b
luna :3 <ellipticobj@gmail.com>
2025-01-10 00:18:37 +0800

    base64

---
+++ b64.py
@@ -0,0 +1,58 @@
+from base64 import *
+import base64
+
+def encode(plain: str) -> str:
+    if not plain:
+        raise ValueError("variable plain cannot be empty")
+    
+    return str(b64encode(plain.encode("utf-8")))
+
+def decode(enc: str, utfdecode: bool = True) -> str:
+    if not enc:
+        raise ValueError("variable enc cannot be empty")
+    
+    return str(b64decode(enc).decode('utf-8')) if utfdecode else str(b64decode(enc))
+
+def inputhandler():
+    while True:
+        print("base64")
+        choice = input('''
+1. encode
+2. decode
+q. quit
+> ''').lower().strip()
+        print()
+        
+        if choice == '1':
+            str = input("input string to encode\n> ")
+            print()
+            
+            return encode(str)
+        
+        elif choice == '2':
+            while True:
+                str = input("input string to decode\n> ")
+                
+                try:
+                    print()
+                    return decode(str)
+                except base64.binascii.Error as e:
+                    print(f"{e}")
+                    print()
+                    continue
+                except UnicodeDecodeError:
+                    continueAlthoughUnicodeError = input(f"the string cannot be decoded by utf-8. return raw bytes? [y/N] ").lower()
+                    if continueAlthoughUnicodeError == "y":
+                        print()
+                        return decode(str, False)
+        
+        elif choice == 'q':
+            print()
+            return "quit"
+        
+        else:
+            print("please input one of the options")
+            print()
+            continue
+        
+print(inputhandler())
---
+++ main.py
@@ -0,0 +1,9 @@
+import b64
+
+while True:
+    choice = input('''1. base64
+2. 
+q. quit
+''').lower().strip()
+    
+    
