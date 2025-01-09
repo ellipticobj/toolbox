613680bf6eda312f2cedf2206da882c339768d03
luna :3 <ellipticobj@gmail.com>
2025-01-10 00:34:25 +0800

    hasher

---
+++ hasher.py
@@ -0,0 +1,39 @@
+import hashlib
+
+def md5(data: str) -> str:
+    return hashlib.md5(data.encode()).hexdigest()
+
+def sha256(data: str) -> str:
+    return hashlib.sha256(data.encode()).hexdigest()
+
+def sha1(data: str) -> str:
+    return hashlib.sha1(data.encode()).hexdigest()
+
+def blake2(data: str) -> str:
+    return hashlib.blake2b(data.encode()).hexdigest()
+
+def ripemd160(data: str) -> str:
+    return hashlib.new("ripemd160", data.encode()).hexdigest()
+
+def inputhandler():
+    while True:
+        choice = input('''1. md5
+2. sha256
+3. sha1
+4. blake2
+5. ripemd160
+> ''')
+        print()
+        data = input("input string to hash: ")
+        
+        if choice == "1": return md5(data)
+        elif choice == "2": return sha256(data)
+        elif choice == "3": return sha1(data)
+        elif choice == "4": return blake2(data)
+        elif choice == "5": return ripemd160(data)
+        else:
+            print("please input a proper input")
+            print()
+            continue
+        
+print(inputhandler())
--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import b64
+import hasher
 
 while True:
     choice = input('''1. base64
 2. 
 q. quit
 ''').lower().strip()
+    print()
     
+    if choice == '1':
+        # base64
+        parsed = b64.inputhandler()
     
+    elif choice == "2":
+        # hash
+        parsed = hasher.inputhandler()
+    
+    elif choice == 'q':
+        print("quitting")
+        break
+    
+    else:
+        print("please input a valid input")
+        continue
+    
+    print(parsed)
+    print()
+    
+
+print("program exited. have a nice day!")
